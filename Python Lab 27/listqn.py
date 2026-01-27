e830c8b1803f37cd6dca8700469881271dd5d646
Amosi Elia Saidi <amosieliasaidi@gmail.com>
2026-01-27 16:12:42 +0300

    Create listqn.py

---
+++ Python Lab 27/listqn.py
@@ -0,0 +1,21 @@
+subjects = ["Math", "Physics", "Chemistry", "Biology", "CS"]
+
+#appendin 
+subjects.append("Art")
+
+#inserting
+subjects.insert(1, "History")
+
+#remove
+subjects.remove("Physics")
+
+#popping
+subjects.pop(3)
+
+#sorting
+subjects.sort()
+
+#Rwversing
+subjects.reverse()
+
+print(subjects)
