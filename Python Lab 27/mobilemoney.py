8ec8b9fa0a072e492ef79b94b4680534780a2d9b
Amosi Elia Saidi <amosieliasaidi@gmail.com>
2026-01-27 16:01:43 +0300

    mobilemoney.py

--- Python Lab 27/mobilemoney.py
+++ Python Lab 27/mobilemoney.py
@@ -1,5 +1,5 @@
 n = input("Dial *150*01# :")
-while n!="*150*01#":
+while n != "*150*01#":
     print("Dialed wrong number")
     n = input("Dial *150*01# :")
     
@@ -7,28 +7,29 @@ def send_money():
     print("SEND MONEY")
     print()
     print(
-        "1.To Same Setwork\n"
+        "1.To Same Network\n"
         "2.To Other Network\n"
-        )
+    )
     
     s = int(input("Enter Choice: "))
     
-    if s==1:
+    if s == 1:
         print("Enter Choice in form of 0712345678")
         pn = int(input("Enter Phone Number: "))
-    elif s==2:
-        net = input("Enetr Network name: ")
+        am = int(input("Enter Amount: "))
+    elif s == 2:
+        net = input("Enter Network name: ")
         print("Enter Choice in form of 0712345678")
         pn = int(input("Enter Phone Number: "))
+        am = int(input("Enter Amount"))
     else:
         print("Invalid input")
-    print("succesfull")
-
+        return pn, net, am# Exit the function if input is invalid
+    
+    print("successful")
 
 print()
-
 print("WELCOME TO MIXX BY YASS USER MENU")
-
 print()
 
 print(
@@ -38,7 +39,10 @@ print(
     "4.Check Balance\n"
     "5.Exit"
     )
-if s==1:
+
+choice = int(input("Enter choice: "))
+
+if choice == 1:
     send_money()
 else:
-    print(no)
+    print("No")
