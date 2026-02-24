7553632af043b7f3d17e3c0a6e196502d9438167
Amosi Elia Saidi <amosieliasaidi@gmail.com>
2026-02-24 20:40:05 +0300

    Update and rename math.py to Qn3_math.py

---
+++ group8assignment2/Qn3_math.py
@@ -0,0 +1,6 @@
+
+import math
+sqrt_age=5
+actual_age=math.pow(sqrt_age,2)
+print(f"the actual age is {actual_age}")
+
--- group8assignment2/math.py
+++
@@ -1,9 +0,0 @@
-"""
-A) Suppose the square root of your age is 5. Write a Java program
-using the Math class to calculate and display your actual age.
-
-"""
-import math
-sqrt_age=5
-actual_age=math.pow(sqrt_age,2)
-print(f"the actual age is {actual_age}")
